81cdded4811fe9ca11f9e1ef0b6d709eef982d9a
Adam M. Intrator <adam.intra2r@gmail.com>
2016-11-07 17:35:08 -0500

    commit num 2

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require ('express');
 const app = express();
 
 
-app.listen(3000, (req, res) => {
-  console.log("Server listening...");
-});
 
 app.get('/', (req, res, next) => {
   res.send("Here's our response.")
+});
+
+app.get('/news', (req, res, next) => {
+  res.send("Hot off the press")
+});
+
+
+
+app.listen(3000, (req, res) => {
+  console.log("Server listening...");
 });
